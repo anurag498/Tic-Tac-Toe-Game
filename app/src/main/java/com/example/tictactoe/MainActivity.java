0bc95a1b035ce53438d567636c77f5950a23efd0
Anurag <goanurag498@gmail.com>
2021-10-01 21:50:11 +0530

    first Commit for Tic Tac Toe Game

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Message;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int activePlayer = 0;
+    int[] setPosition = {2,2,2,2,2,2,2,2,2};
+    int[][] winningPos = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    boolean gameActive = true;
+    public void playAgain(View view){
+
+        Button play = findViewById(R.id.button);
+        Button cancel = findViewById(R.id.button2);
+        play.setVisibility(View.INVISIBLE);
+        cancel.setVisibility(View.INVISIBLE);
+        GridLayout gl = findViewById(R.id.gridLayout);
+        for(int i = 0; i < gl.getChildCount(); i++) {
+            ImageView posi = (ImageView) gl.getChildAt(i);
+            posi.setImageDrawable(null);
+        }
+        int activePlayer = 0;
+        for(int i = 0;i<setPosition.length;i++) {
+            setPosition[i] = 2;
+        }
+        gameActive = true;
+    }
+
+
+    public void position(View view){
+        ImageView posi = (ImageView) view;
+
+        int target = Integer.parseInt(posi.getTag().toString());
+        if(setPosition[target] == 2 && gameActive)
+        {
+            posi.setTranslationY(-1500);
+           setPosition[target] = activePlayer;
+           if(activePlayer == 0) {
+              activePlayer = 1;
+              posi.setImageResource(R.drawable.blue);
+           }
+           else{
+            activePlayer = 0;
+            posi.setImageResource(R.drawable.red);
+           }
+           posi.animate().translationYBy(1500).alpha(1).setDuration(300);
+           String winner = "";
+           for(int[] winPos : winningPos) {
+                if (setPosition[winPos[0]] == setPosition[winPos[1]] && setPosition[winPos[1]] == setPosition[winPos[2]] && setPosition[winPos[0]] != 2) {
+                   if (activePlayer == 1) {
+                    winner = "Blue won the match";
+
+                    }
+                   else if (activePlayer == 0) {
+                    winner = "Red won the match";
+                    }
+                gameActive = false;
+                Toast.makeText(this, Html.fromHtml("<big><b>"+winner+"</b><big>"), Toast.LENGTH_SHORT).show();
+                    Button play = findViewById(R.id.button);
+                    Button cancel = findViewById(R.id.button2);
+                    play.setVisibility(View.VISIBLE);
+                    cancel.setVisibility(View.VISIBLE);
+                }
+
+           }
+        }
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
